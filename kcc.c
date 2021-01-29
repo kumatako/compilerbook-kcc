1aa551ee999d5aa55b8a69051bae60890e193a51
kumatako <61956254+kumatako@users.noreply.github.com>
2021-01-29 16:34:57 +0900

    ステップ4: エラーメッセージを改良

--- kcc.c
+++ kcc.c
@@ -32,6 +32,22 @@ void error(char *fmt, ...){
 	exit(1);
 }
 
+char *user_input;
+
+/// エラー個所を報告する
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr,"\n");
+	exit(1);
+}
+
 /**
 次のトークンが期待している記号のときには、トークンを一つ読み進めて真を返す。
 それ以外の場合には偽を返す。
@@ -49,7 +65,7 @@ bool consume(char op){
 */
 void expect(char op){
 	if(token->kind != TK_RESERVED || token->str[0] != op)
-		error("'%c'ではありません",op);
+		error_at(token->str, "'%c'ではありません",op);
 	token = token->next;
 }
 
@@ -60,7 +76,7 @@ void expect(char op){
 */
 int expect_number() {
 	if(token->kind != TK_NUM) 
-		error("数ではありません");
+		error_at(token->str, "数ではありません");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -102,7 +118,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 		
-		error("トークナイズできません");
+		error_at(p,"トークナイズできません");
 	}
 	
 	new_token(TK_EOF, cur, p);
@@ -115,6 +131,7 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
+	user_input = argv[1];
 	token = tokenize(argv[1]);
 	
 	printf(".intel_syntax noprefix\n");
